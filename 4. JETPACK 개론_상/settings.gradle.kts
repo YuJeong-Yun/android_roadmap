11bf57259ef1f0cef43f4818b0e24ab50fce8844
YUJEONG YUN <yunyj99@naver.com>
2024-01-22 23:13:32 +0900

    5-1-1) findViewById / Kotlin Extensions

---
+++ 4. JETPACK 개론_상/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ch4_JETPACK_1"
+include(":app")
