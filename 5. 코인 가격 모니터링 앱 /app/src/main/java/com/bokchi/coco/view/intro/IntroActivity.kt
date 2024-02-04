2daa1bed87fbed2c5b1ac05d53c600f31930b56d
YUJEONG YUN <yunyj99@naver.com>
2024-02-04 14:51:52 +0900

    6-1-2) 스플래시 화면 만들기 (SplashScreen)

---
+++ 5. 코인 가격 모니터링 앱 /app/src/main/java/com/bokchi/coco/view/intro/IntroActivity.kt
@@ -0,0 +1,28 @@
+package com.bokchi.coco.view.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bokchi.coco.R
+import timber.log.Timber
+
+/**
+ *  Splash 화면 만들기
+ *  기존 방식 : handler 이용 -> 3초 뒤에 다른액티비(MainActivity로 이동)
+ *  Splash Screen 을 이용 -> https://developer.android.com/develop/ui/views/launch/splash-screen
+ *      theme.xml 에서 splash 관련 설정 지정
+ *      AndroidManifest.xml 에서 인트로 액티비티에 android:theme로 지정
+ */
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 인트로 액티비티에 아래 코드 추가
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        Timber.d("onCreate")
+
+    }
+}
