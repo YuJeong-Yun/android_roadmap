22a19dcfaebd1b50879e3f9bdcd37581a6e2cc6f
YUJEONG YUN <yunyj99@naver.com>
2024-02-05 02:15:06 +0900

    6-1-13) 코인 선택화면 Adapter (RecyclerView)

--- 5. 코인 가격 모니터링 앱 /app/src/main/java/com/bokchi/coco/view/SelectActivity.kt
+++ 5. 코인 가격 모니터링 앱 /app/src/main/java/com/bokchi/coco/view/SelectActivity.kt
@@ -12,7 +12,6 @@ import timber.log.Timber
 
 // 현재가 정보 조회 api
 // https://apidocs.bithumb.com/reference/%ED%98%84%EC%9E%AC%EA%B0%80-%EC%A0%95%EB%B3%B4-%EC%A1%B0%ED%9A%8C-all
-
 class SelectActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivitySelectBinding
--- 5. 코인 가격 모니터링 앱 /app/src/main/java/com/bokchi/coco/view/adapter/SelectRVAdapter.kt
+++ 5. 코인 가격 모니터링 앱 /app/src/main/java/com/bokchi/coco/view/adapter/SelectRVAdapter.kt
@@ -15,7 +15,8 @@ import timber.log.Timber
 class SelectRVAdapter(val context : Context, val coinPriceList : List<CurrentPriceResult>)
     :RecyclerView.Adapter<SelectRVAdapter.ViewHolder>(){
 
-    val selectedCoinList = ArrayList<String>()
+    // 좋아요한 코인 목록 담을 리스트
+    private val selectedCoinList = ArrayList<String>()
 
     inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
         val coinName : TextView = view.findViewById(R.id.coinName)
@@ -24,17 +25,16 @@ class SelectRVAdapter(val context : Context, val coinPriceList : List<CurrentPri
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         val view = LayoutInflater.from(parent.context).inflate(R.layout.intro_coin_item, parent, false)
 
         return ViewHolder(view)
-
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        // 코인 이름
         holder.coinName.text = coinPriceList[position].coinName
 
+        // 상승,하락 여부
         val fluctate_24H = coinPriceList[position].coinInfo.fluctate_24H
 
         if(fluctate_24H.contains("-")) {
@@ -45,9 +45,11 @@ class SelectRVAdapter(val context : Context, val coinPriceList : List<CurrentPri
             holder.coinPriceUpDown.setTextColor(Color.parseColor("#ed2e11"))
         }
 
+        // 하트 이미지
         val likeImage = holder.likeImage
         val currentCoin = coinPriceList[position].coinName
 
+        // 리사이클러뷰는 뷰를 재활용하므로 아래 처리 필요!
         // view 를 그려줄 때
         if(selectedCoinList.contains(currentCoin)) {
             likeImage.setImageResource(R.drawable.like_red)
@@ -56,7 +58,6 @@ class SelectRVAdapter(val context : Context, val coinPriceList : List<CurrentPri
         }
 
         likeImage.setOnClickListener {
-
             Timber.d(currentCoin)
 
             if(selectedCoinList.contains(currentCoin)) {
@@ -68,10 +69,7 @@ class SelectRVAdapter(val context : Context, val coinPriceList : List<CurrentPri
                 selectedCoinList.add(currentCoin)
                 likeImage.setImageResource(R.drawable.like_red)
             }
-
             Timber.d(selectedCoinList.toString())
-
-
         }
 
 
