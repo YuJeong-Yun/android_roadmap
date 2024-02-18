7e05ce9255b258bd6572fb3d63a757c90021661c
YUJEONG YUN <yunyj99@naver.com>
2024-02-18 23:23:14 +0900

    6-3) 파일 추가

---
+++ 5. 코인 가격 모니터링 앱 /app/src/main/java/com/bokchi/coco/view/main/CoinListFragment.kt
@@ -0,0 +1,44 @@
+package com.bokchi.coco.view.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bokchi.coco.R
+import com.bokchi.coco.databinding.FragmentCoinListBinding
+import com.bokchi.coco.db.entity.InterestCoinEntity
+import timber.log.Timber
+
+
+class CoinListFragment : Fragment() {
+
+    private var _binding : FragmentCoinListBinding? = null
+    private val binding get() = _binding!!
+
+    private val selectedList = ArrayList<InterestCoinEntity>()
+    private val unSelectedList = ArrayList<InterestCoinEntity>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+
+        _binding = FragmentCoinListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+
+    }
+
+
+}
---
+++ 5. 코인 가격 모니터링 앱 /app/src/main/java/com/bokchi/coco/view/main/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.bokchi.coco.view.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.bokchi.coco.db.entity.InterestCoinEntity
+import com.bokchi.coco.repository.DBRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val dbRepository = DBRepository()
+
+    lateinit var selectedCoinList : LiveData<List<InterestCoinEntity>>
+
+    // CoinListFragment
+
+    fun getAllInterestCoinData() = viewModelScope.launch {
+
+        val coinList = dbRepository.getAllInterestCoinData().asLiveData()
+        selectedCoinList = coinList
+
+    }
+
+    fun updateInterestCoinData(interestCoinEntity: InterestCoinEntity) = viewModelScope.launch(Dispatchers.IO) {
+
+        if(interestCoinEntity.selected) {
+            interestCoinEntity.selected = false
+        } else {
+            interestCoinEntity.selected = true
+        }
+
+        dbRepository.updateInterestCoinData(interestCoinEntity)
+
+    }
+
+
+    // PriceChangeFragment
+
+}
